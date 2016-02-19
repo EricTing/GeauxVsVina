0ca55d3ece4860f2dfb859d7bd3c9378cb354bd8
EricTing <yunding.whu@gmail.com>
2016-02-19 11:55:31 -0600

    add inputs for testing
    	new file:   1h6v_NDP_B_1.pdb.pdbqt
    	new file:   1h6v_NDP_B_1.pdb.pdbqt.vina.pdbqt
    	new file:   1h6v_NDP_B_1.pdb.pdbqt.vina.sdf
    	new file:   1h6v_NDP_B_1.pdb.sdf
    	modified:   test_restore_order.py

--- test_restore_order.py
+++ test_restore_order.py
@@ -2,44 +2,32 @@
 
 import unittest
 
-from glob import glob
 from restore_order import getPredConfs, getOrderedSdf
 
-my_id = '1a07C1'
-vina_ifn = '/work/jaydy/working/InitialSdf/' + my_id + '_init.pdbqt'
-init_sdf_fn = "/work/jaydy/working/InitialSdf/" + my_id + "_init.sdf"
-vina_out_dir = '/home/jaydy/work/working/VinaResult/' + my_id
-vina_out_ofns = glob(vina_out_dir + '/*-out.pdbqt')
-
-init_sdf = [l.rstrip() for l in file(init_sdf_fn)]
-vina_in = [l.rstrip() for l in file(vina_ifn)]
+VINA_IFN = "./1h6v_NDP_B_1.pdb.pdbqt"
+INIT_SDF_IFN = "./1h6v_NDP_B_1.pdb.sdf"
+VINA_PRED_PDBQT = "./1h6v_NDP_B_1.pdb.pdbqt.vina.pdbqt"
+VINA_PRED_SDF = "./1h6v_NDP_B_1.pdb.pdbqt.vina.sdf"
 
 
 class TestCentroid(unittest.TestCase):
-    def test_case1(self):
+    def test_case(self):
+        init_sdf = [l.rstrip() for l in file(INIT_SDF_IFN)]
+        vina_in = [l.rstrip() for l in file(VINA_IFN)]
         all_atom_zones = []
-        for vina_ofn in vina_out_ofns:
-            vina_out = [l.rstrip() for l in file(vina_ofn)]
-            pred_confs = getPredConfs(vina_out)
-            all_atom_zones += pred_confs
-
-    def test_case2(self):
-        all_atom_zones = []
-        for vina_ofn in vina_out_ofns:
-            vina_out = [l.rstrip() for l in file(vina_ofn)]
-            pred_confs = getPredConfs(vina_out)
-            all_atom_zones += pred_confs
+        vina_out = [l.rstrip() for l in file(VINA_PRED_PDBQT)]
+        pred_confs = getPredConfs(vina_out)
+        all_atom_zones += pred_confs
 
         ordered_sdfs = []
         for atom_zone in all_atom_zones:
             ordered_sdfs.append(getOrderedSdf(init_sdf, atom_zone, vina_in))
 
-        for idx, sdf in enumerate(ordered_sdfs):
-            vina_sdf_ofn = vina_out_dir + '/' + my_id + '_' + str(idx) + '.sdf'
-            with open(vina_sdf_ofn, 'w') as f:
-                for l in sdf:
-                    f.write(l + "\n")
-            print "writing to", vina_sdf_ofn
+        with open(VINA_PRED_SDF, 'w') as ofs:
+            for sdf in ordered_sdfs:
+                for line in sdf:
+                    ofs.write(line)
+                    ofs.write("\n")
 
 
 if __name__ == "__main__":
